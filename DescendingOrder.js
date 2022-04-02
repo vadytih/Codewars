419a1eb8fcd81225b293fc35793fb9832d6c2af3
Vadim <vady.tih@gmail.com>
2022-04-02 18:41:56 +0300

    Codewars DescendingOrder

---
+++ DescendingOrder.js
@@ -0,0 +1,48 @@
+// Ваша задача состоит в том, чтобы создать функцию, которая может принимать любое неотрицательное целое число в качестве аргумента и возвращать его с цифрами в порядке убывания. По сути, переставьте цифры, чтобы получить максимально возможное число.
+//
+//     Примеры:
+// Вход: 42145 Выход:54421
+//
+// Вход: 145263 Выход:654321
+//
+// Вход: 123456789 Выход:987654321
+
+function descendingOrder(n){
+
+    let str = String(n);
+    let result = "";
+    let numsObj = {}
+
+
+    for(let i = 0; i < str.length; i++) {
+
+        if ( numsObj.hasOwnProperty(str[i]) ) {
+            numsObj[str[i]].push(str[i]);
+        } else {
+            numsObj[str[i]] = [str[i]]
+        }
+    }
+
+    for (key in numsObj) {
+        result += numsObj[key].join("")
+    }
+
+    result = result.split("").reverse()
+    return result = Number(result.join(""))
+
+}
+
+//Tests
+
+const { assert } = require("chai")
+
+describe("Basic tests", () => {
+    it("Testing for fixed tests", () => {
+        assert.strictEqual(descendingOrder(0), 0)
+        assert.strictEqual(descendingOrder(1), 1)
+        assert.strictEqual(descendingOrder(111), 111)
+        assert.strictEqual(descendingOrder(15), 51)
+        assert.strictEqual(descendingOrder(1021), 2110)
+        assert.strictEqual(descendingOrder(123456789), 987654321)
+    })
+})
